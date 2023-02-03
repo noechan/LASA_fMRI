b820790051c3a908fa3d78cff03a4ee1a343db3a
Noelia Martinez Molina <noelia.martinezmolina@gmail.com>
2023-02-03 15:18:53 +0100

    moved

--- singing_performance/startup.m
+++
@@ -1,5 +0,0 @@
-
-%startup file
-data_path='/Volumes/LASA/Aphasia_project/tb-fMRI/recordings';
-code_path='/Volumes/LASA/Aphasia_project/tb-fMRI/code_project/singing_performance';
-addpath(['/Volumes/LASA/Aphasia_project/tb-fMRI/code_project/singing_performance/functions']);
