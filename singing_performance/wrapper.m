ad87ff4852f1d0dd2f304e8d40a0f1515be7c56f
Noelia Martinez Molina <noelia.martinezmolina@gmail.com>
2023-02-08 16:09:14 +0100

    added

--- singing_performance/wrapper.m
+++
@@ -1,7 +0,0 @@
-% NB: Remember to edit manually the inputs cohort, song and session to be handled by the scripts
-startup
-get_audio_loge_zc
-singing_performance_baseline
-singing_performance_listen
-singing_performance_singalong
-singing_performance_singmem
